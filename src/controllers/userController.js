76fc59c68c807720f447177b0b3626e7b44fab18
Nguyen Van Sinh <nguyenvansinh1812@gmail.com>
2024-06-28 23:01:20 +0700

    react api

---
+++ src/controllers/userController.js
@@ -0,0 +1,22 @@
+import userService from "../services/userServices";
+let handleLogin = async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  if (!email || !password) {
+    return res.status(500).json({
+      errCode: 1,
+      msg: "Email or password is required",
+    });
+  }
+  let userData = await userService.handleUserLogin(email, password);
+
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.errMessage,
+    user: userData.user ? userData.user : {},
+  });
+};
+
+module.exports = {
+  handleLogin: handleLogin,
+};
